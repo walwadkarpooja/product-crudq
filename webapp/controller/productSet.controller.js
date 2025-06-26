bba949e3bdf2a3930e6ff881ca589ae19c9f0b00
Puja Walwadkar <pooja.walwadkar1@gmail.com>
2025-06-26 04:39:23 +0000

    product set update

--- webapp/controller/productSet.controller.js
+++ webapp/controller/productSet.controller.js
@@ -1,32 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("crudqproduct.controller.productSet", {
         onInit() {
-            // let editModel = new sap.ui.model.json.JSONModel({
-            //     isEditableField : false
-            // });
-            // this.getView().setModel(editModel,"editModel");
+            let oViewModel = new sap.ui.model.json.JSONModel({
+                Productid: "",
+            })
+            this.getView().setModel(oViewModel, "jModel")
+
+            let editModel = new sap.ui.model.json.JSONModel({
+                isEditableField: false,
+                isEnabledField: false,
+            })
+            this.getView().setModel(editModel,'editModel')
+            
         },
         onSubmit: function(oEvent){
             
             let productId = oEvent.getParameter("value");
-            console.log('here we clicked',productId);
+           
             let oModel = this.getOwnerComponent().getModel();
            
             let sPath = "/productSet('"+productId+"')";
             oModel.read(sPath , {
-                success: function(oData,Response){
+                success: function(oData,oHeaders){
                     let jModel = new sap.ui.model.json.JSONModel(oData);
                     this.getView().setModel(jModel,'jModel');
-                    console.log('jModel',jModel);
+                    console.log(oHeaders)
+                    MessageToast.show(oHeaders.statusText)                    
                     // this.getView().byId('prodVisible').setVisible(true)
                     // this.getView().getModel("editModel").setProperty("/isEditableField",true)
                     
-                }.bind(this)
+                }.bind(this),
+                error: function(error){
+                    // console.log(error.message);
+                    MessageToast.show(error.message)
+                }
             })
+        },
+        onEdit: function (){
+            let oModel = this.getView().getModel('editModel');
+            oModel.setProperty("/isEditableField",true);
+            oModel.setProperty("/isEnabledField",true);
         }
     });
 });
